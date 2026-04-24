46815ea1d89721fc20543333f9dea86ecb99ecd6
Anna Ilyushkina <annailiushkina15@gmail.com>
2026-04-24 16:49:33 +0300

    add class Apple

--- the_snake.py
+++ the_snake.py
@@ -55,15 +55,47 @@ class GameObject:
             self.position = (320, 240)
         else:
             self.position = position
-    
+   
     def draw(self, surface):
         """
         Абстрактный метод для отрисовки объекта на экране.
         Аргумент: surface (поверность, на которой рисуем)
         """
-
     pass
 
+
+class Apple(GameObject):
+    """
+    Класс Apple. Наследуется от GameObject.
+    Появляется в случайном месте поля.
+    """
+
+    super().__init__(position=None, body_color=APPLE_COLOR)
+    
+    self.radomize_position()
+    
+    def randomize_position(self):
+        """
+        Устанавливает случайные координаты для яблока.
+        """
+        max_x = 640 - 20
+        max_y = 480 - 20
+        x.random.randrage(0, max_x + 1, 20)
+        y.random.randrage(0, max_y + 1, 20)
+        
+        self.position = (x, y)
+    
+    def draw(self, surface):
+        """
+        Отрисовывает яблоко на игровом поле.
+        """
+
+        rect = pygame.Rect(
+            self.position[0],
+            self.position[1],
+            20,
+            20
+        )
 
 def main():
     # Инициализация PyGame:
