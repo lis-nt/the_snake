241db18f2c0c323d08778492d286161181e68a5d
Anna Ilyushkina <annailiushkina15@gmail.com>
2026-04-29 22:04:08 +0300

    class Apple changes

--- the_snake.py
+++ the_snake.py
@@ -70,20 +70,16 @@ class Apple(GameObject):
     Класс Apple. Наследуется от GameObject.
     Появляется в случайном месте поля.
     """
-
-    super().__init__(position=None, body_color=APPLE_COLOR)
-    
-    self.radomize_position()
+    def __init__(self):
+        super().__init__(body_color=APPLE_COLOR)
+    self.randomize_position()
     
     def randomize_position(self):
         """
         Устанавливает случайные координаты для яблока.
         """
-        max_x = 640 - 20
-        max_y = 480 - 20
-        x.random.randrage(0, max_x + 1, 20)
-        y.random.randrage(0, max_y + 1, 20)
-        
+        x = randint(0, GRID_WIDTH - 1) * GRID_SIZE
+        y = randint(0, GRID_HEIGHT - 1) * GRID_SIZE
         self.position = (x, y)
     
     def draw(self, surface):
@@ -94,9 +90,11 @@ class Apple(GameObject):
         rect = pygame.Rect(
             self.position[0],
             self.position[1],
-            20,
-            20
+            GRID_SIZE,
+            GRID_SIZE
         )
+        pygame.draw.rect(surface, self.body_color, rect)
+        pygame.draw.rect(surface, BORDER_COLOR, rect, 1)
 
 def main():
     # Инициализация PyGame:
